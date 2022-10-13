2423abc1ca342149d5362a3c401987cf02e29942
Fernando Jimenez <fgjv06@gmail.com>
2022-10-13 13:41:01 -0300

    Pixel Data

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const img = new Image();
-
-
 const reader = new FileReader();
-
-const imageLoader = document.getElementById("uploader");
 
 function uploadImage(e) {
   reader.readAsDataURL(e.target.files[0]);
@@ -16,7 +12,11 @@ function uploadImage(e) {
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img, 0, 0);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+console.log(imageData);
   };
   }
 }
+
+const imageLoader = document.getElementById("uploader");
 imageLoader.addEventListener("change", uploadImage);
