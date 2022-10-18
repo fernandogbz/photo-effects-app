a5df9d58b5a35bacd71f0d3d4778f5039c2a70a1
Fernando Jimenez <fgjv06@gmail.com>
2022-10-18 13:24:36 -0300

    Greyscale, Sepia and Invert functions created

--- script.js
+++ script.js
@@ -18,16 +18,42 @@ function uploadImage(e) {
 const imageLoader = document.getElementById("uploader");
 imageLoader.addEventListener("change", uploadImage);
 
-const change = () => {
+function greyscale(){
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   const data = imageData.data;
   for (let i = 0; i < data.length; i += 4) {
-    data[i] = 255;
-    data[i + 1];
-    data[i + 2];
+    const grey = data[i] * 0.21 + data[i + 1] * 0.71 + data[i + 2] * 0.07;
+    data[i] = grey;
+    data[i + 1] = grey;
+    data[i + 2] = grey;
   }
   ctx.putImageData(imageData, 0, 0);
 }
 
-document.querySelectorAll("button")[0].addEventListener("click", change);
+function sepia(){
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const grey = data[i] * 0.21 + data[i + 1] * 0.71 + data[i + 2] * 0.07;
+    data[i] = grey + 95;
+    data[i + 1] = grey + 58;
+    data[i + 2] = grey;
+  }
+  ctx.putImageData(imageData, 0, 0);
+}
+
+function invert(){
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i];
+    data[i + 1] = 255 - data[i+1];
+    data[i + 2] = 255 - data[i+2];
+  }
+  ctx.putImageData(imageData, 0, 0);
+}
+
+document.querySelectorAll("button")[0].addEventListener("click", greyscale);
+document.querySelectorAll("button")[1].addEventListener("click", sepia);
+document.querySelectorAll("button")[2].addEventListener("click", invert);
 
